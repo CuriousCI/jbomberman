8c5a4aa09233338f823d27a965c4ec559f98f019
Ionut Cicio <ionutcicio.dev@gmail.com>
2024-04-16 10:42:47 +0200

    Added docstrings to User record

--- src/jbomberman/model/User.java
+++ src/jbomberman/model/User.java
@@ -1,5 +1,17 @@
 package jbomberman.model;
 
 // TODO: icon avatar
+/* 
+ * @author SomebodyThatIUsedToKnow
+ * 
+ * @param nickname Some cringe nickname
+ * @param games You play too much, go outside
+ *
+ * <pre>
+ * {@code
+ * var user = new User("BomberUser23", 10, 7, 5)
+ * }
+ * </pre>
+ * */
 public record User(String nickname, int games, int victories, int level) {
 };
